03019396058cfd931db9d3227b54072f9a408a6e
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-02-15 21:25:51 +0100

    Handle async operations

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,34 +1,22 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
+import axios from "axios";
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
 
-const initialState = [
-  {
-    id: "1",
-    title: "Learning Redux Toolkit",
-    content: "I've heard good things.",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-  {
-    id: "2",
-    title: "Slices...",
-    content: "The more I say slice, the more I want pizza.",
-    date: sub(new Date(), { minutes: 2 }).toISOString(),
-    reactions: {
-      thumbsUp: 1,
-      wow: 0,
-      heart: 3,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-];
+const initialState = {
+  posts: [],
+  status: "idle",
+  error: null,
+};
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  try {
+    const { data } = await axios.get(POSTS_URL);
+    return data;
+  } catch (error) {
+    return error.message;
+  }
+});
 
 const postSlice = createSlice({
   name: "posts",
@@ -36,7 +24,7 @@ const postSlice = createSlice({
   reducers: {
     addPost: {
       reducer(state, action) {
-        state.unshift(action.payload);
+        state.posts.unshift(action.payload);
       },
       prepare(title, content, userId) {
         return {
@@ -59,17 +47,48 @@ const postSlice = createSlice({
     },
     addReaction(state, action) {
       const { postId, reaction } = action.payload;
-      const existingPost = state.find(post => post.id === postId);
+      const existingPost = state.posts.find(post => post.id === postId);
 
       if (existingPost) {
         existingPost.reactions[reaction]++;
       }
     },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        let min = 1;
+        // cuz out fake api doesn't have those data
+        const loadedPosts = action.payload.map(post => {
+          post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          post.reactions = {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+          };
+          return post;
+        });
+
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(loadedPosts);
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
 });
 
-//To be more flex, imagine we use posts in more than one component and we have const post = useSelector(tate => state.posts), if the state structure change we will have to change it in each place, this approach makes that we will need to change it only here
-export const selectAllPosts = state => state.posts;
+//To be more flex, imagine we use posts in more than one component and we have const post = useSelector(state => state.posts.posts), if the state structure change we will have to change it in each place, this approach makes that we will need to change it only here
+export const selectAllPosts = state => state.posts.posts;
+export const getPostsStatus = state => state.posts.status;
+export const getPostsError = state => state.posts.error;
 
 export const { addPost, addReaction } = postSlice.actions;
 
