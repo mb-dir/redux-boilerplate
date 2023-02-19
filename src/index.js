f0880ec7a80a11a754247b5512a5bc6087796516
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-02-19 22:28:01 +0100

    Install and basic setup of react router

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { fetchUsers } from "./features/users/usersSlice";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //get usres as soon as app loads
 store.dispatch(fetchUsers());
@@ -13,7 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </Provider>
   //</React.StrictMode>
 );
